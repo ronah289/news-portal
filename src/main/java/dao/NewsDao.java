2c222175f08bd296056e38ee50f4128bb8418f6d
ronah289 <ronald.kibet@student.moringaschool.com>
2021-11-14 15:47:26 -0900

    define data access objects for models

---
+++ src/main/java/dao/NewsDao.java
@@ -0,0 +1,20 @@
+package dao;
+
+import models.DepartmentNews;
+import models.News;
+
+import java.util.List;
+
+public interface NewsDao {
+
+    //create
+    void addNews(News news);
+    void addDepartmentNews(DepartmentNews department_news);
+
+    //read
+    List<News> getAll();
+    News findById(int id);
+
+    //delete
+    void clearAll();
+}
---
+++ src/main/java/dao/UsersDao.java
@@ -0,0 +1,20 @@
+package dao;
+
+import models.Departments;
+import models.Users;
+
+import java.util.List;
+
+public interface UsersDao {
+
+    //create
+    void  add(Users user);
+
+    //read
+    List<Users> getAll();
+    List<Departments> getAllUserDepartments(int user_id);
+    Users findById(int id);
+
+    //delete
+    void clearAll();
+}
